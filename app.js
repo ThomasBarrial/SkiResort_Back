cdb590c9398230b1e17b7c22288907a8e107bd77
Thomas <barrialthomas@gmail.com>
2021-07-23 13:16:19 +0200

    maj

--- app.js
+++ app.js
@@ -5,8 +5,6 @@ const cors = require('cors');
 
 const port = process.env.PORT || 3000;
 
-
-
 connection.connect((err) => {
   if (err) {
     console.error('error connecting: ' + err.stack);
@@ -18,8 +16,8 @@ connection.connect((err) => {
 app.use(express.json());
 app.use(cors());
 
-app.get('/api/MyWave/', (req, res) => {
-  connection.query('SELECT * FROM SurfSpot', (err, result) => {
+app.get('/api/skiresort/', (req, res) => {
+  connection.query('SELECT * FROM Resorts', (err, result) => {
     if (err) {
       res.status(500).send('Error retrieving data from database');
     } else {
@@ -28,71 +26,64 @@ app.get('/api/MyWave/', (req, res) => {
   });
 });
 
-app.get('/api/MyWave/:id', (req, res) => {
-  const spotId = req.params.id
- 
-connection.query('SELECT * FROM SurfSpot WHERE id = ?' ,[spotId], (err, result) => {
-  
-if (err) {  
-  res.status(500).send('Error retrieving data from database');
-} else {
-  res.status(200).json(result);
-}
-});
-});
+app.get('/api/skiresort/:id', (req, res) => {
+  const resortId = req.params.id;
 
-
-app.post('/api/MyWave/', (req, res) => {
-  const {  name, photo, description, webcam, condition, niveaux_ID, break_ID, lieu, acces_ID } = req.body
-  connection.query(
-    'INSERT INTO `SurfSpot` (`name`, `photo`, `description`, `webcam`, `condition`, `niveaux_ID`, `break_ID`, `lieu`, `acces_ID`) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)',
-    [name, photo, description, webcam, condition, niveaux_ID,  break_ID, lieu, acces_ID],
-    (err, result) => {
-      if(err) {
-        res.status(500).send('Error saving the Spot');
-        console.log(name)
-        console.log(req.body)
-        console.log(err)
-      } else { 
-        console.log(req.body)
-        res.status(201).send('Spot successfully saved')
-      }
-    }
-   )
-})
-
-app.put('/api/MyWave/:id', (req, res) => {
-  const spotId = req.params.id
-  console.log(spotId)
-  const userPropsToUpdate = req.body;
-  console.log(req.body)
-
-  connection.query('UPDATE SurfSpot SET ? WHERE id = ?', [userPropsToUpdate, spotId], (err) => { if(err) {
-    console.log(err)
-    res.status(500).send('Error updating a user');
-  } else {
-    res.status(200).send('User updated successfully 🎉');
-  }});
-});
-
-
-app.delete('/api/Mywave/:id', (req, res) => {
-  const spotID = req.params.id
-  console.log(spotID);
-  connection.query('DELETE FROM SurfSpot WHERE id = ?', [spotID], (err) => {
-    if(err) {
-      console.log(err)
-      res.status(500).send('Error deleting files')
+  connection.query('SELECT * FROM Resorts WHERE id = ?', [resortId], (err, result) => {
+    if (err) {
+      res.status(500).send('Error retrieving data from database');
     } else {
-      res.status(200).send('Delete success')
+      res.status(200).json(result[0]);
     }
-  })
-})
+  });
+});
 
+app.post('/api/skiresort/', (req, res) => {
+  const { name, Description, siteWeb, condition, images, massifID, scopeID, typeID, Altitude } = req.body;
+  connection.query(
+    'INSERT INTO `Resorts` (`name`, `Description`, `siteWeb`, `condition`, `images`, `massifID`, `scopeID`, `typeID`, `Altitude` ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)',
+    [name, Description, siteWeb, condition, images, massifID, scopeID, typeID, Altitude],
+    (err, result) => {
+      if (err) {
+        res.status(500).send('Error saving the Spot');
+        console.log(name);
+        console.log(req.body);
+        console.log(err);
+      } else {
+        console.log(req.body);
+        res.status(201).send('Spot successfully saved');
+      }
+    },
+  );
+});
+
+app.put('/api/skiresort/:id', (req, res) => {
+  const resortId = req.params.id;
+  const resortPropsToUpdate = req.body;
+  console.log(req.body);
+
+  connection.query('UPDATE Resorts SET ? WHERE id = ?', [resortPropsToUpdate, resortId], (err) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send('Error updating a user');
+    } else {
+      res.status(200).send('User updated successfully 🎉');
+    }
+  });
+});
+
+app.delete('/api/skiresort/:id', (req, res) => {
+  const resortID = req.params.id;
+  connection.query('DELETE FROM Resorts WHERE id = ?', [resortID], (err) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send('Error deleting files');
+    } else {
+      res.status(200).send('Delete success');
+    }
+  });
+});
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
-
-
-
